32c5d587a5b6fb252563a5cab7fa628779f24ec3
znxuyifan <znxuyifan@163.com>
2025-05-12 13:33:12 +0800

    update

--- nets/2.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-# 加载权重文件
-weights_path ='C:\experiment_project\infrared_small_traget\\test\\75\weight\\76.pth'
-checkpoint = torch.load(weights_path, map_location='cpu')
-
-# 打印权重文件中的键
-print("Keys in the checkpoint:")
-print(checkpoint.keys())
